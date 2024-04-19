7126d08f40b024db775c6012323618dca2088093
jsoptimizer <jsoptimizer.pro@gmail.com>
2024-04-19 01:05:47 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+function App() {
+
+  return (<main className='w-screen h-screen flex items-center justify-center bg-neutral font-poppins text-brand-950'>shorten url webapp</main>)
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    colors:{
+      brand:{
+        DEFAULT:'hsl(180, 66%, 49%)',
+        950:"hsl(257, 27%, 26%)"
+      },
+      secondary:"hsl(0, 87%, 67%)",
+      neutral:{
+        DEFAULT:"hsl(0, 0%, 75%)",
+        100:"hsl(257, 7%, 63%)",
+        // 200:"hsl(257, 7%, 63%)",
+        // 300:"hsl(257, 7%, 63%)",
+        // 400:"hsl(257, 7%, 63%)",
+        // 500:"hsl(257, 7%, 63%)",
+        // 600:"hsl(257, 7%, 63%)",
+        // 700:"hsl(257, 7%, 63%)",
+        800:"hsl(255, 11%, 22%)",
+        // 900:"hsl(257, 7%, 63%)",
+        950:"hsl(260, 8%, 14%)"
+      }
+    },
+    extend: {
+      fontFamily:{
+        'poppins':['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
